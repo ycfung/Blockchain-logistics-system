1eb5c21a68b1d47439777b82128a3c6d27e336c7
Chingfung Yung <rcf@hotmail.co.th>
2019-10-18 22:58:25 +0800

    update django view

--- django_test/django_test/view.py
+++ django_test/django_test/view.py
@@ -1,11 +1,38 @@
 from django.http import HttpResponse
+from django_test.sawtooth_client.test import *
+import json
 
 
 def hello(request):
     # 此处根据请求内容调用test.py中的相应方法
-    if(request.method == "POST"):
+    if (request.method == "POST"):
         # request.POST 返回 QueryDict 对象
-        print(request.POST)
-    elif(request.method == "GET"):
-        print(request.GET)
-    return HttpResponse(request)
+        function = request.POST['func']
+        f = {'status': 'success', 'data': 'this is data you POST'}
+        try:
+            if (len(request.POST) == 2):
+                f = str(getattr(method, function)(request.POST['arg1']))
+            elif (len(request.POST) == 3):
+                f = str(getattr(method, function)(request.POST['arg1'], request.POST['arg2']))
+            elif (len(request.POST) == 4):
+                f = str(getattr(method, function)(request.POST['arg1'], request.POST['arg2']), request.POST['arg3'])
+        except TypeError:
+            f = {'status': 'failed', 'message': 'Wrong argument(s)'}
+        return HttpResponse(json.dumps(f), content_type='application/json')
+
+
+    elif request.method == "GET":
+        function = request.GET['func']
+        f = {'status': 'success', 'message': 'this is a success message'}
+        try:
+            if (len(request.GET) == 1):
+                f = getattr(method, function)(method)
+            if (len(request.GET) == 2):
+                f = str(getattr(method, function)(request.GET['arg1']))
+            elif (len(request.GET) == 3):
+                f = str(getattr(method, function)(request.GET['arg1'], request.GET['arg2']))
+            elif (len(request.GET) == 4):
+                f = str(getattr(method, function)(request.GET['arg1'], request.GET['arg2']), request.GET['arg3'])
+        except TypeError:
+            f = {'status': 'failed', 'message': 'Wrong argument(s)'}
+        return HttpResponse(json.dumps(f), content_type='application/json')
